fba68bad09e5ce6e9c99da7ac2e96a774d6c828c
0xbe1 <0xbetrue@gmail.com>
2022-12-16 14:34:08 +0800

    feat: faucet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,18 @@
-import type { AppProps } from 'next/app'
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+import { PontemWallet } from "@pontem/wallet-adapter-plugin";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <AptosWalletAdapterProvider
+        plugins={[new PetraWallet(), new PontemWallet()]}
+        autoConnect={true}
+      >
+        <Component {...pageProps} />
+      </AptosWalletAdapterProvider>
+    </ChakraProvider>
+  );
 }
