49aa090ee681c6c55683a1b533e5cea4dbdeb691
0xbe1 <0xbetrue@gmail.com>
2022-12-28 12:10:36 +0800

    feat: add martian wallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { PetraWallet } from "petra-plugin-wallet-adapter";
 import { PontemWallet } from "@pontem/wallet-adapter-plugin";
 import type { AppProps } from "next/app";
+import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <AptosWalletAdapterProvider
-        plugins={[new PetraWallet(), new PontemWallet()]}
+        plugins={[new PetraWallet(), new PontemWallet(), new MartianWallet()]}
         autoConnect={true}
       >
         <Component {...pageProps} />
