e3a0b098337daf310c0d226837ce5c07bd46356d
0xbe1 <0xbetrue@gmail.com>
2022-12-29 06:11:39 +0800

    feat: trust, rise, and spika wallets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,22 @@ import { PetraWallet } from "petra-plugin-wallet-adapter";
 import { PontemWallet } from "@pontem/wallet-adapter-plugin";
 import type { AppProps } from "next/app";
 import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
+import { RiseWallet } from "@rise-wallet/wallet-adapter";
+import { TrustWallet } from "@trustwallet/aptos-wallet-adapter";
+import { SpikaWallet } from "@spika/aptos-plugin";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <AptosWalletAdapterProvider
-        plugins={[new PetraWallet(), new PontemWallet(), new MartianWallet()]}
+        plugins={[
+          new PetraWallet(),
+          new PontemWallet(),
+          new MartianWallet(),
+          new RiseWallet(),
+          new TrustWallet(),
+          new SpikaWallet(),
+        ]}
         autoConnect={true}
       >
         <Component {...pageProps} />
